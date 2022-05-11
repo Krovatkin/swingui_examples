e85805dbca3e3649dd950ac949cdc9aacffad519
Nikolay Korovaiko <korovaikon@gmail.com>
2022-05-10 22:24:33 -0700

    TextDemo

---
+++ TextDemo.java
@@ -0,0 +1,79 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class TextDemo extends JPanel implements ActionListener {
+    protected JTextField textField;
+    protected JTextArea textArea;
+    private final static String newline = "\n";
+
+    public TextDemo() {
+        super(new GridBagLayout());
+
+        // We encourage you to specify the number of columns for each text field. 
+        // If you do not specify the number of columns or a preferred size, 
+        // then the field's preferred size changes whenever the text changes, 
+        // which can result in unwanted layout updates.
+
+        textField = new JTextField(20);
+        textField.addActionListener(this);
+
+        textArea = new JTextArea(5, 20);
+        textArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+
+        //Add Components to this panel.
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridwidth = GridBagConstraints.REMAINDER;
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        add(textField, c);
+
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 1.0;
+        c.weighty = 1.0;
+        add(scrollPane, c);
+    }
+
+    public void actionPerformed(ActionEvent evt) {
+        // The text returned by this method does not include a newline character 
+        // for the Enter key that fired the action event.
+        String text = textField.getText();
+        // reset text
+        textField.setText("");
+        textArea.append(text + newline);
+        textField.selectAll();
+
+        //Make sure the new text is visible, even if there
+        //was a selection in the text area.
+        textArea.setCaretPosition(textArea.getDocument().getLength());
+    }
+
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event dispatch thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("TextDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add contents to the window.
+        frame.add(new TextDemo());
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event dispatch thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
